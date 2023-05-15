3473798253cb5442fb6bced764904e859f62d3d9
or.maymon <ormay30@gmail.com>
2023-05-15 14:27:22 +0300

    connect to sqlserver db and added a table

---
+++ BookItWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookItWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookItWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
