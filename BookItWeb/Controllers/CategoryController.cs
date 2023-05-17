47e190407278ed9d7030ee9f78c83915e44e5d3c
or.maymon <ormay30@gmail.com>
2023-05-17 12:20:33 +0300

    added all the crud op

--- BookItWeb/Controllers/CategoryController.cs
+++ BookItWeb/Controllers/CategoryController.cs
@@ -27,17 +27,77 @@ namespace BookItWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             if (obj.DisplayOrder.Equals(0))
             {
-                ModelState.Remove("DisplayOrder");
+                ModelState.Remove("DisplayOrder"); //couldn't use clearvalidation //need to check why
                 ModelState.AddModelError("DisplayOrder", "please enter a number");
 
             }
 
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null) {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category update successfully";
+                return RedirectToAction("Index");
+            }
+
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _db.Categories.Find(id); 
+            if (obj == null) { 
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+
+        }
+
+
+
 
     }
 }
