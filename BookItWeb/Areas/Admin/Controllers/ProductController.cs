dc74cd46725161c57ed15293211fa197869738ee
or.maymon <ormay30@gmail.com>
2023-05-25 12:51:53 +0300

    Fixed a bug with Edit when uploading img combine Create and Edit page to Upsert

--- BookItWeb/Areas/Admin/Controllers/ProductController.cs
+++ BookItWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,10 @@
 ﻿using BookIt.DataAccess.Repository.IRepository;
 using BookIt.Models;
+using BookIt.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.IO;
 
 namespace BookItWeb.Areas.Admin.Controllers
 {
@@ -8,60 +12,76 @@ namespace BookItWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _UnitOfWork;
+        private readonly IWebHostEnvironment _WebHostEnvironment;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork,IWebHostEnvironment webHostEnvironment)
         {
             _UnitOfWork = unitOfWork;
+            _WebHostEnvironment = webHostEnvironment; 
         }
 
         public IActionResult Index()
         {
             List<Product> objProductList = _UnitOfWork.Product.GetAll().ToList();
+            
             return View(objProductList);
         }
 
-        public IActionResult Create() 
+        public IActionResult Upsert(int? id) 
         {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Create(Product product) {
-            if(ModelState.IsValid)
+            ProductVM productVM = new()
             {
-                _UnitOfWork.Product.Add(product);
+                CategoryList= _UnitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Product = new Product()
+            };
+            if(id == null ||id == 0) 
+            {
+                return View(productVM);
+            }
+            else //update
+            {
+                productVM.Product = _UnitOfWork.Product.Get(u=> u.Id == id);
+                return View(productVM);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(ProductVM obj, IFormFile? file) {
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _WebHostEnvironment.WebRootPath;
+                if(file != null) 
+                { 
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using(var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    obj.Product.ImageUrl= @"\images\product"+fileName;
+                    
+                }
+                _UnitOfWork.Product.Add(obj.Product);
                 _UnitOfWork.Save();
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
             }
-            return View();
-        }
-
-        public IActionResult Edit(int? id) 
-        {
-            if (id == null || id == 0)
+            else 
             {
-                return NotFound();
-            }
-            Product? productFromDb = _UnitOfWork.Product.Get(u => u.Id == id);
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if(ModelState.IsValid)
-            {
-                _UnitOfWork.Product.Update(obj);
-                _UnitOfWork.Save();
-                TempData["success"] = "Product update successfully";
-                return RedirectToAction("Index");
+                obj.CategoryList = _UnitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+                return View(obj);
 
             }
-            return View();
         }
 
         public IActionResult Delete(int? id) 
